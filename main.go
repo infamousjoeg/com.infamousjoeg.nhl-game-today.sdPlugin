e8f503448625322475e8a2b19cd1c01960283011
Joe Garcia <infamousjoeg@users.noreply.github.com>
2022-11-13 18:43:13 -0500

    Update main.go

--- main.go
+++ main.go
@@ -9,11 +9,7 @@ import (
 func main() {
 	// Team ID 14 = Tampa Bay Lightning
 	scheduleResponse := nhlgametoday.GetSchedule(14)
-	awayTeamID := scheduleResponse.Dates[0].Games[0].Teams.Away.Team.ID
-	homeTeamID := scheduleResponse.Dates[0].Games[0].Teams.Home.Team.ID
-	awayTeam := nhlgametoday.GetTeam(awayTeamID)
-	homeTeam := nhlgametoday.GetTeam(homeTeamID)
-	awayTeamShortName := awayTeam.Teams[0].Abbreviation
-	homeTeamShortName := homeTeam.Teams[0].Abbreviation
-	fmt.Println(awayTeamShortName, "at", homeTeamShortName)
+	awayTeam := nhlgametoday.GetTeam(scheduleResponse.Dates[0].Games[0].Teams.Away.Team.ID)
+	homeTeam := nhlgametoday.GetTeam(scheduleResponse.Dates[0].Games[0].Teams.Home.Team.ID)
+	fmt.Println(awayTeam.Teams[0].Abbreviation, "at", homeTeam.Teams[0].Abbreviation)
 }
